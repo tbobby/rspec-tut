befac7a462dbfd318197c06bb57c0b305d1cb557
tbobby <tbobby@git>
2012-06-07 20:52:42 +0700

    Initial setup with rails new <app> -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecTut::Application.config.secret_token = 'c2a1ccf738491e4e2fe69806e75330971abfc0f5657cdaedf48781c3f37a1a27162a430cda6865202c5d0938d43e3962322adc763c0f5b9af069962d703bb3eb'
