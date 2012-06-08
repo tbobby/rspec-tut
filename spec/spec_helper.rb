91209ee64fabc3476ca7c3d5383b9073493f3497
tbobby <tbobby@git>
2012-06-09 06:57:44 +0700

    Added failing test for 'about' page.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,18 +9,17 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  unless defined?(Rails)
-    require File.dirname(__FILE__) + '/../config/environment.rb'
-    require 'rspec'
-    require 'rspec/rails'
-  end
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
-  
+
+  unless defined?(Rails)
+    require File.dirname(__FILE__) + '/../config/environment.rb'
+  end
+
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
