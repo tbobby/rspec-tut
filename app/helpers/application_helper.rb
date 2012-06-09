fdb624075c62ff8057cbe2738bdf21377361ca32
tbobby <tbobby@git>
2012-06-09 19:34:51 +0700

    Fixed syntax error for if-else-alt statement.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   # Returns a title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
-    @title.nil ? base_title : "#{base_title} | #{@title}"
+    @title.nil? ? base_title : "#{base_title} | #{@title}"
   end
 end
