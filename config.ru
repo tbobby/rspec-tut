be3be867aa4bd65b3d4ca71d3a4830382fe1f8d2
tbobby <tbobby@git>
2012-06-10 22:35:56 +0700

    Performance tips from blog.railsonfire.com/2012/05/18/Assets-Sprites-CDN.html

--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
+
+# Compress all assets and pages before serving
+use Rack::Deflater
 run RspecTut::Application
--- config/compass.rb
+++ config/compass.rb
@@ -1,2 +1,9 @@
 # Require any additional compass plugins here.
 project_type = :rails
+
+# To reduce CSS output
+line_comments = false
+
+# Sprites should be put into public/assets so 
+# they can be accessed via the asset pipeline
+generated_images_dir = "public/assets"
